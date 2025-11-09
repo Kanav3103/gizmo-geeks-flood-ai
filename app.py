8756cbceef937c56b54d3e1f518e125031ef71e0
Kanav3103 <169050903+Kanav3103@users.noreply.github.com>
2025-11-09 16:31:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,6 @@ import folium
 from streamlit_folium import st_folium
 import os
 
-st.write(list(df.columns))  # or print(list(df.columns))
-
 # =====================================
 # 🔸 LOAD REAL DATA & TRAIN MODEL
 # =====================================

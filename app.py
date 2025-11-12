f1a1b9ec95ee2544c713bf4fdfe803ee7f20113a
Kanav3103 <169050903+Kanav3103@users.noreply.github.com>
2025-11-12 21:02:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ h1, h2, h3, h4, h5, h6 {
 
 /* ===== Subtle Text Glow Effect ===== */
 h1, h2, h3, h4, h5, h6, p, span, div {
-    text-shadow: 0 0 6px rgba(210, 190, 255, 1.00),
-                 0 0 14px rgba(180, 140, 230, 0.90);
+    text-shadow: 0 0 10px rgba(210, 190, 255, 0.60),
+                 0 0 18px rgba(180, 140, 230, 0.50);
 }
 
 </style>
